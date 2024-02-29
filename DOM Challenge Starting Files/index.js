a2ae421a168761c9fff15da68224ea7f71c92f7f
mooham <mooham2541@hotmail.com>
2024-02-29 11:48:22 +0700

    First commit

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,13 @@
+let googleLink = document.querySelector("ul a");
+let thirdElement = document.querySelector("ul").lastElementChild;
+let btn = document.querySelector("button");
+let h1 = document.querySelector("h1");
+
+thirdElement.innerHTML = "ham";
+
+googleLink.style.color = "orange";
+
+btn.style.backgroundColor = "pink";
+
+h1.classList.add("huge");
+
